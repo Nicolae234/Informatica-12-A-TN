d39016463fd8915cb20f7b8ba83c4ad3aa2ad2dd
Turcan Nicolae <144727181+Nicolae234@users.noreply.github.com>
2026-01-16 01:34:42 +0200

    Implement Fibonacci sequence calculation

--- Modelare_Matematica/Problema4.py
+++ Modelare_Matematica/Problema4.py
@@ -1,1 +1,14 @@
+n = int(input("Introduceți n: "))
+if n == 1:
+    a_n = 1
+elif n == 2:
+    a_n = 2
+else:
+    a_prev2 = 1
+    a_prev1 = 2  
+    for i in range(3, n + 1):
+        a_n = a_prev1 + a_prev2
+        a_prev2 = a_prev1
+        a_prev1 = a_n
 
+print("Elementul a_n este:", a_n)
